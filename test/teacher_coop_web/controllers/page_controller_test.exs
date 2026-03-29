b5c0e54ba90c5d0ac151b109335aff65acb42404
Emmanuel Guefif <eguefif@fastmail.com>
2026-03-29 10:06:42 +0200

    Initial commit

---
+++ test/teacher_coop_web/controllers/page_controller_test.exs
@@ -0,0 +1,8 @@
+defmodule TeacherCoopWeb.PageControllerTest do
+  use TeacherCoopWeb.ConnCase
+
+  test "GET /", %{conn: conn} do
+    conn = get(conn, ~p"/")
+    assert html_response(conn, 200) =~ "Peace of mind from prototype to production"
+  end
+end
