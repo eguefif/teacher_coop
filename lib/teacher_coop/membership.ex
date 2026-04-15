b9f3d23a2bd38af8a964d42fcbd96a2bc711c882
Emmanuel Guefif <eguefif@fastmail.com>
2026-04-15 20:55:04 +0200

    Add table memberhips

---
+++ lib/teacher_coop/membership.ex
@@ -0,0 +1,20 @@
+defmodule TeacherCoop.Membership do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "memberships" do
+    field :role, :string
+    field :working_group_id, :id
+    field :user_id, :id
+
+    timestamps(type: :utc_datetime)
+  end
+
+  @doc false
+  def changeset(membership, attrs, user_scope) do
+    membership
+    |> cast(attrs, [:role])
+    |> validate_required([:role])
+    |> put_change(:user_id, user_scope.user.id)
+  end
+end
---
+++ priv/repo/migrations/20260415185439_create_memberships.exs
@@ -0,0 +1,17 @@
+defmodule TeacherCoop.Repo.Migrations.CreateMemberships do
+  use Ecto.Migration
+
+  def change do
+    create table(:memberships) do
+      add :role, :string
+      add :working_group_id, references(:working_groups, on_delete: :nothing)
+      add :user_id, references(:users, type: :id, on_delete: :delete_all)
+
+      timestamps(type: :utc_datetime)
+    end
+
+    create index(:memberships, [:user_id])
+
+    create index(:memberships, [:working_group_id])
+  end
+end
