c8cfa582a527f7d5d60b23e957c39288212d2649
Emmanuel Guefif <eguefif@fastmail.com>
2026-06-25 10:20:04 +0200

    Add seed

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,34 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+alias TeacherCoop.Library
+alias TeacherCoop.Accounts
+
+# TODO: Create a user
+
+user_attrs = %{:email => "rob@rob.org"}
+
+{:ok, user} = Accounts.register_user(user_attrs)
+
+user_scope = Accounts.Scope.for_user(user)
+
+# TODO: Create 3 documents for this user
+
+attrs = [
+  %{
+    :title => "Fraction en ligne",
+    :description => "Some fraction description"
+  },
+  %{
+    :title => "Les verbes pronominaux",
+    :description => "Séance de conjugaison"
+  },
+  %{
+    :title => "Le système métrique",
+    :description => "Présentation du système métrique et son histoire."
+  }
+]
+
+attrs
+|> Enum.map(&Library.create_document(user_scope, &1))
