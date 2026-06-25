663ce4e4f738bde57d19000b79075718ebce37c4
Emmanuel Guefif <eguefif@fastmail.com>
2026-06-25 09:23:58 +0200

    Refactor context: divide into two subcontext
    We split the Library context into two subcontext to separate concern:
    * documents for crude operations
    * search for indexing/search operations
    
    The goal is to separate concerns. We have the same context but two
    concerns.

--- lib/teacher_coop/library.ex
+++ lib/teacher_coop/library.ex
@@ -1,147 +1,22 @@
 defmodule TeacherCoop.Library do
   @moduledoc """
   The Library context.
+  The Library handles documents in two ways:
+    - CRUDE operations handled by the sub context Library.Documents
+    - Search operations handled by the sub context Library.Search
   """
 
-  import Ecto.Query, warn: false
-  alias TeacherCoop.Repo
+  alias TeacherCoop.Library.{Documents}
 
-  alias TeacherCoop.Library.Document
-  alias TeacherCoop.Accounts.Scope
+  # Delegates functions for CRUDE operations ****************************************
 
-  @doc """
-  Subscribes to scoped notifications about any document changes.
+  defdelegate subscribe_documents(scope), to: Documents
+  defdelegate list_documents(scope), to: Documents
+  defdelegate get_document!(scope, id), to: Documents
+  defdelegate create_document(scope, attrs), to: Documents
+  defdelegate update_document(scope, document, attrs), to: Documents
+  defdelegate delete_document(scope, document), to: Documents
+  defdelegate change_document(scope, document, attrs), to: Documents
 
-  The broadcasted messages match the pattern:
-
-    * {:created, %Document{}}
-    * {:updated, %Document{}}
-    * {:deleted, %Document{}}
-
-  """
-  def subscribe_documents(%Scope{} = scope) do
-    key = scope.user.id
-
-    Phoenix.PubSub.subscribe(TeacherCoop.PubSub, "user:#{key}:documents")
-  end
-
-  defp broadcast_document(%Scope{} = scope, message) do
-    key = scope.user.id
-
-    Phoenix.PubSub.broadcast(TeacherCoop.PubSub, "user:#{key}:documents", message)
-  end
-
-  @doc """
-  Returns the list of documents.
-
-  ## Examples
-
-      iex> list_documents(scope)
-      [%Document{}, ...]
-
-  """
-  def list_documents(%Scope{} = scope) do
-    Repo.all_by(Document, user_id: scope.user.id)
-  end
-
-  @doc """
-  Gets a single document.
-
-  Raises `Ecto.NoResultsError` if the Document does not exist.
-
-  ## Examples
-
-      iex> get_document!(scope, 123)
-      %Document{}
-
-      iex> get_document!(scope, 456)
-      ** (Ecto.NoResultsError)
-
-  """
-  def get_document!(%Scope{} = scope, id) do
-    Repo.get_by!(Document, id: id, user_id: scope.user.id)
-  end
-
-  @doc """
-  Creates a document.
-
-  ## Examples
-
-      iex> create_document(scope, %{field: value})
-      {:ok, %Document{}}
-
-      iex> create_document(scope, %{field: bad_value})
-      {:error, %Ecto.Changeset{}}
-
-  """
-  def create_document(%Scope{} = scope, attrs) do
-    with {:ok, document = %Document{}} <-
-           %Document{}
-           |> Document.changeset(attrs, scope)
-           |> Repo.insert() do
-      broadcast_document(scope, {:created, document})
-      {:ok, document}
-    end
-  end
-
-  @doc """
-  Updates a document.
-
-  ## Examples
-
-      iex> update_document(scope, document, %{field: new_value})
-      {:ok, %Document{}}
-
-      iex> update_document(scope, document, %{field: bad_value})
-      {:error, %Ecto.Changeset{}}
-
-  """
-  def update_document(%Scope{} = scope, %Document{} = document, attrs) do
-    true = document.user_id == scope.user.id
-
-    with {:ok, document = %Document{}} <-
-           document
-           |> Document.changeset(attrs, scope)
-           |> Repo.update() do
-      broadcast_document(scope, {:updated, document})
-      {:ok, document}
-    end
-  end
-
-  @doc """
-  Deletes a document.
-
-  ## Examples
-
-      iex> delete_document(scope, document)
-      {:ok, %Document{}}
-
-      iex> delete_document(scope, document)
-      {:error, %Ecto.Changeset{}}
-
-  """
-  def delete_document(%Scope{} = scope, %Document{} = document) do
-    true = document.user_id == scope.user.id
-
-    with {:ok, document = %Document{}} <-
-           Repo.delete(document) do
-      broadcast_document(scope, {:deleted, document})
-      {:ok, document}
-    end
-  end
-
-  @doc """
-  Returns an `%Ecto.Changeset{}` for tracking document changes.
-
-  ## Examples
-
-      iex> change_document(scope, document)
-      %Ecto.Changeset{data: %Document{}}
-
-  """
-  def change_document(%Scope{} = scope, %Document{} = document, attrs \\ %{}) do
-    true = document.user_id == scope.user.id
-
-    Document.changeset(document, attrs, scope)
-  end
+  # Delegates functions for search operations ***************************************
 end
---
+++ lib/teacher_coop/library/search.ex
@@ -0,0 +1,7 @@
+defmodule TeacherCoop.Library.Search do
+  @moduledoc """
+  Sub Library context for search.
+
+  We use Meilisearch, this context allows for indexing/searching documents.
+  """
+end
