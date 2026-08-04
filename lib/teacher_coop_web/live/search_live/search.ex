55def89532ea74230516de3f8928a14851af6b8a
Emmanuel Guefif <97660236+eguefif@users.noreply.github.com>
2026-08-04 10:30:17 +0200

    Improve search layout (#15)

--- lib/teacher_coop_web/live/search_live/search.ex
+++ lib/teacher_coop_web/live/search_live/search.ex
@@ -1,17 +1,16 @@
 defmodule TeacherCoopWeb.SearchLive.Search do
   use TeacherCoopWeb, :live_view
   alias TeacherCoop.Discovery
-  alias TeacherCoopWeb.Date
 
   @impl true
   def render(assigns) do
     ~H"""
     <Layouts.app flash={@flash} current_scope={@current_scope}>
-      <div class="flex flex-col items-center">
+      <div class="flex flex-col items-center gap-[64px]">
         <form
           phx-change="update-search"
           phx-submit="trigger-search"
-          class="flex flex-col gap-4 items-center"
+          class="flex flex-row gap-[48px] items-baseline"
         >
           <.input
             id="search"
@@ -31,29 +30,37 @@ defmodule TeacherCoopWeb.SearchLive.Search do
             </.button>
           </div>
         </form>
-        <div :if={@results != nil} class="max-w-200">
-          <ul :for={result <- @results} class="list w-200">
-            <li class="list-row mb-4">
-              <div class="flex flex-col gap-4">
-                <div class="flex flex-row gap-4 items-baseline">
-                  <div class="text-lg">
-                    <.link
-                      class="btn-ghost"
-                      navigate={~p"/documents/#{result["id"]}?return_to=search"}
-                    >
-                      {result["title"]}
-                    </.link>
-                  </div>
-                  - <span class="">{result["inserted_at"] |> Date.format_time()}</span>
-                </div>
-                <div class="text-xs font-semibold uppercase">{result["institution_type"]}</div>
-                <div class="text-base text-justify">{result["description"]}</div>
-              </div>
-            </li>
-          </ul>
+        <div :if={@results != nil} class="max-w-200 flex flex-col gap-[64px]">
+          <div :for={result <- @results} class="w-200">
+            <.result result={result} />
+          </div>
         </div>
       </div>
+      <pre><%= inspect @results, pretty: true %></pre>
     </Layouts.app>
+    """
+  end
+
+  attr :result, :map
+
+  def result(assigns) do
+    ~H"""
+    <div class="flex flex-col gap-4">
+      <div class="flex flex-row gap-4 items-baseline">
+        <div class="text-lg">
+          <.link
+            class="btn-ghost hover:underline"
+            navigate={~p"/documents/#{@result["id"]}?return_to=search"}
+          >
+            <div class="text-primary text-xl">{@result["title"]}</div>
+          </.link>
+        </div>
+      </div>
+      <div class="text-md font-semibold uppercase opacity-60">
+        {@result["institution_type"]} - {@result["grade"]}
+      </div>
+      <div class="text-base text-justify">{@result["description"]}</div>
+    </div>
     """
   end
 
@@ -62,11 +69,16 @@ defmodule TeacherCoopWeb.SearchLive.Search do
     scope =
       if Map.has_key?(socket.assigns, :current_scope), do: socket.assigns.current_scope, else: nil
 
+    {:ok, results} =
+      Discovery.create_search(scope, %{
+        search_terms: "fraction"
+      })
+
     {:ok,
      socket
      |> assign_new(:current_scope, fn -> scope end)
-     |> assign(:search_terms, "")
-     |> assign(:results, nil)}
+     |> assign(:search_terms, "fraction")
+     |> assign(:results, results.hits)}
   end
 
   @impl true
